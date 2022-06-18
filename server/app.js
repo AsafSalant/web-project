a3fc4f6aeb72d7016a2d3d12d6a80edc6be776f8
Asaf Salant <asaf@skeep.io>
2022-06-18 13:51:50 +0300

    step 2

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 app.use(express.static('../client',{extensions: ['html', 'htm']}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post("/booking",(req,res) => {
+    console.log(req.body);
+});
 
 app.listen(port, (req,res) => {
     console.log('Goood run')

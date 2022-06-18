4101a403b04550343b29ac35b665a50130eedfc6
Asaf Salant <asaf@skeep.io>
2022-06-18 14:59:19 +0300

    step 5

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 
 const uri = "mongodb+srv://asaf-salant:Sal85208520@web-project.bvep98x.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
-client.connect();
+
 const app = express();
 const port = 3000;
 
@@ -13,24 +13,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const database = client.db('web-project');
-const bookings = database.collection('bookings');
+const bookingsColl = database.collection('bookings');
 
 app.post("/booking",(req,res) => {
     console.log(req.body);
 });
 
-async function run() {
-    try {
-        const query = { name: 'asaf' };
-        const booking = await bookings.findOne(query);
-        console.log(booking);
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-}
-
-run().catch(console.dir);
+app.get("/booking/:name", async (req,res) => {
+    await client.connect();
+    const listOfBookings = await bookingsColl.findOne({name:req.params.name});
+    await client.close()
+    res.send(listOfBookings)
+});
 
 app.listen(port, (req,res) => {
     console.log('Good run')

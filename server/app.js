fafb30ad2ded317988206de4d1f19a8df743331f
Asaf Salant <asaf@skeep.io>
2022-06-18 09:01:43 +0300

    sone change

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('../public',{extensions: ['html', 'htm']}))
 
 app.listen(port, (req,res) => {
     console.log('Goood run')
-})
+});

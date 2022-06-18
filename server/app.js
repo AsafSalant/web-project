74dcb9f5c5a12552c1b9d5deb92c5e756899de2d
Asaf Salant <asaf@skeep.io>
2022-06-18 14:16:55 +0300

    mogodb server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const app = express()
 const { MongoClient } = require("mongodb");
+
 const uri = "mongodb+srv://asaf-salant:Sal85208520@web-project.bvep98x.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 client.connect();
-
+const app = express();
 const port = 3000;
 
 app.use(express.static('../client',{extensions: ['html', 'htm']}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const database = client.db('web-project');
 const bookings = database.collection('bookings');
@@ -22,15 +21,16 @@ app.post("/booking",(req,res) => {
 
 async function run() {
     try {
-      const query = { name: 'asaf' };
-      const booking = await bookings.findOne(query);
-      console.log(booking);
+        const query = { name: 'asaf' };
+        const booking = await bookings.findOne(query);
+        console.log(booking);
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+
+run().catch(console.dir);
 
 app.listen(port, (req,res) => {
     console.log('Good run')

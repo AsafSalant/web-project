c50c180b26e7b4eda9515a585795c6518918bc8e
Asaf Salant <asaf@skeep.io>
2022-06-18 14:08:38 +0300

    step 3

--- server/app.js
+++ server/app.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://asaf-salant:Sal85208520@web-project.bvep98x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+client.connect();
+
+const port = 3000;
 
 app.use(express.static('../client',{extensions: ['html', 'htm']}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+const database = client.db('web-project');
+const bookings = database.collection('bookings');
+
 app.post("/booking",(req,res) => {
     console.log(req.body);
 });
 
+async function run() {
+    try {
+      const query = { name: 'asaf' };
+      const booking = await bookings.findOne(query);
+      console.log(booking);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 app.listen(port, (req,res) => {
-    console.log('Goood run')
+    console.log('Good run')
 });
